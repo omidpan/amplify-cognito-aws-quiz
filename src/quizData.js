a4ec1558ba43ea49440f5d1871056844e044e49d
omidpan <omidpnteaching@gmail.com>
2024-05-28 15:15:19 -0400

    add label for later use case

--- src/quizData.js
+++ src/quizData.js
@@ -1,108 +1,124 @@
 const quizData = [
     {
-        "question": "What AI stands for?",
-        "options": ["Air Isolation ", "Army Innovation", "Automated intelligence", "Artificial Intelligence"],
-        "answer": "Artificial Intelligence"
-      },
-      {
-        question: "Do you know who founded BitHubAi company?",
-          options: ["Omid Panahi","Mark B. Fisher", "Satoshi Nakamoto", "Jeff Bozos"],
-          answer: "Omid Panahi" 
-        },
-      {
-        "question": "What is the goal of AI?",
-        "options": ["To understand human emotions", "To mimic human intelligence", "To build rockets", "To grow vegetables"],
-        "answer": "To mimic human intelligence"
-      },
-    {
-        "question": "Why do we use AI?",
-        "options": ["To play games", "To make coffee", "To automate tasks", "To bake cakes"],
-        "answer": "To automate tasks"
-      },
-      {
-        "question": "Which one makes more sense when using AI ?",
-        "options": ["Cooking recipes", "Driving cars", "Singing songs", "Flying kites"],
-        "answer": "Driving cars"
-      },
-      {
-        question: "Who painted the Mona Lisa?",
-        options: ["Vincent Van Gogh", "Leonardo da Vinci", "Pablo Picasso", "Claude Monet"],
-        answer: "Leonardo da Vinci"
-      },
-      {
-        "question": "Which problem an Artificial Intelligence tries to solve?",
-        "options": ["By making us smarter", "By predicting weather", "By washing dishes", "By ordering pizza"],
-        "answer": "By predicting weather"
-      },
-      {
-        "question": "What is the purpose of AI?",
-        "options": ["To make us laugh", "To cure diseases", "To build sandcastles", "To teach mathematics"],
-        "answer": "To cure diseases"
-      },
-      {
-        question: "Which of these companies was not founded in a garage?",
-        options: ["Amazon", "Google", "Apple", "Microsoft"],
-        answer: "Amazon"
-      },
-   
-    {
-        "question": "What do we call a computer program that learns from data?",
-        "options": ["AI", "ML", "DL", "RL"],
-        "answer": "ML"
-      },
-      {
-        "question": "Which of the following is an example of Learning from data?",
-        "options": ["Calculator", "Toaster", "Robot", "Television"],
-        "answer": "Robot"
-      },
-      {
-        question: "Which city is the France Capital?",
-        options: ["Nance", "Paris", "London", "Amersterdam"],
-        answer: "Paris"
-      },
-      {
-        "question": "Which of the following is an example of automated assistance in daily life?",
-        "options": ["Microwave", "Refrigerator", "Smartphone", "Pencil"],
-        "answer": "Smartphone"
-      },
-    
-      {
-        "question": "What do we call the mysterious regions in space where gravity is so strong that not even light can escape?",
-        "options": ["White holes", "Dark matter", "Black holes", "Supernovas"],
-        "answer": "Black holes"
-      },
-    {
-      question: "Which of these companies has radical human mimic program?",
-      options: ["Amazon", "Google", "Apple", "Microsoft"],
-      answer: "Google"
+      "label": 1,
+      "question": "What AI stands for?",
+      "options": ["Air Isolation ", "Army Innovation", "Automated intelligence", "Artificial Intelligence"],
+      "answer": "Artificial Intelligence"
     },
     {
-        "question": "In the field of quantum mechanics, what term describes the state where a particle can exist in multiple states simultaneously?",
-        "options": ["Quantum leap", "Quantum entanglement", "Quantum superposition", "Quantum tunneling"],
-        "answer": "Quantum superposition"
-      },  
-    {
-      question: "What is the capital of France?",
-      options: ["New York", "London", "Paris", "Dublin"],
-      answer: "Paris"
-    },
-   
-    {
-      question: "What is the largest planet in our solar system?",
-      options: ["Earth", "Jupiter", "Saturn", "Mars"],
-      answer: "Jupiter"
+      "label": 2,
+      "question": "Do you know who founded BitHubAi company?",
+      "options": ["Omid Panahi","Mark B. Fisher", "Satoshi Nakamoto", "Jeff Bozos"],
+      "answer": "Omid Panahi" 
     },
     {
-        question: "Did you answer a same question twice?",
-        options: ["No","Yes","May be","I don't know"],
-        answer: "Yes"
-      },
-    {
-        question: "do you think I am a smart AI application asking you these questions based on your general information ?",
-      options: ["yes", "No"],
-      answer: "yes" 
+      "label": 3,
+      "question": "What is the goal of AI?",
+      "options": ["To understand human emotions", "To mimic human intelligence", "To build rockets", "To grow vegetables"],
+      "answer": "To mimic human intelligence"
     },
-
+    {
+      "label": 4,
+      "question": "Why do we use AI?",
+      "options": ["To play games", "To make coffee", "To automate tasks", "To bake cakes"],
+      "answer": "To automate tasks"
+    },
+    {
+      "label": 5,
+      "question": "Which one makes more sense when using AI?",
+      "options": ["Cooking recipes", "Driving cars", "Singing songs", "Flying kites"],
+      "answer": "Driving cars"
+    },
+    {
+      "label": 6,
+      "question": "Who painted the Mona Lisa?",
+      "options": ["Vincent Van Gogh", "Leonardo da Vinci", "Pablo Picasso", "Claude Monet"],
+      "answer": "Leonardo da Vinci"
+    },
+    {
+      "label": 7,
+      "question": "Which problem does Artificial Intelligence try to solve?",
+      "options": ["By making us smarter", "By predicting weather", "By washing dishes", "By ordering pizza"],
+      "answer": "By predicting weather"
+    },
+    {
+      "label": 8,
+      "question": "What is the purpose of AI?",
+      "options": ["To make us laugh", "To cure diseases", "To build sandcastles", "To teach mathematics"],
+      "answer": "To cure diseases"
+    },
+    {
+      "label": 9,
+      "question": "Which of these companies was not founded in a garage?",
+      "options": ["Amazon", "Google", "Apple", "Microsoft"],
+      "answer": "Amazon"
+    },
+    {
+      "label": 10,
+      "question": "What do we call a computer program that learns from data?",
+      "options": ["AI", "ML", "DL", "RL"],
+      "answer": "ML"
+    },
+    {
+      "label": 11,
+      "question": "Which of the following is an example of Learning from data?",
+      "options": ["Calculator", "Toaster", "Robot", "Television"],
+      "answer": "Robot"
+    },
+    {
+      "label": 12,
+      "question": "Which city is the capital of France?",
+      "options": ["Nance", "Paris", "London", "Amsterdam"],
+      "answer": "Paris"
+    },
+    {
+      "label": 13,
+      "question": "Which of the following is an example of automated assistance in daily life?",
+      "options": ["Microwave", "Refrigerator", "Smartphone", "Pencil"],
+      "answer": "Smartphone"
+    },
+    {
+      "label": 14,
+      "question": "What do we call the mysterious regions in space where gravity is so strong that not even light can escape?",
+      "options": ["White holes", "Dark matter", "Black holes", "Supernovas"],
+      "answer": "Black holes"
+    },
+    {
+      "label": 15,
+      "question": "Which of these companies has a radical human mimic program?",
+      "options": ["Amazon", "Google", "Apple", "Microsoft"],
+      "answer": "Google"
+    },
+    {
+      "label": 16,
+      "question": "In the field of quantum mechanics, what term describes the state where a particle can exist in multiple states simultaneously?",
+      "options": ["Quantum leap", "Quantum entanglement", "Quantum superposition", "Quantum tunneling"],
+      "answer": "Quantum superposition"
+    },
+    {
+      "label": 17,
+      "question": "What is the capital of France?",
+      "options": ["New York", "London", "Paris", "Dublin"],
+      "answer": "Paris"
+    },
+    {
+      "label": 18,
+      "question": "What is the largest planet in our solar system?",
+      "options": ["Earth", "Jupiter", "Saturn", "Mars"],
+      "answer": "Jupiter"
+    },
+    {
+      "label": 19,
+      "question": "Did you answer the same question twice?",
+      "options": ["No","Yes","Maybe","I don't know"],
+      "answer": "Yes"
+    },
+    {
+      "label": 20,
+      "question": "Do you think I am a smart AI application asking you these questions based on your general information?",
+      "options": ["Yes", "No"],
+      "answer": "Yes" 
+    }
   ];
   
   export default quizData;
